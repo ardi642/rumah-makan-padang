0f6b331248b0039074914b353dedf3987861e703
Ardiansyah Latif <ardiansyahlatif6420@gmail.com>
2023-07-19 17:24:56 +0700

    memperbarui file database .sql

--- rumah_makan.sql
+++ rumah_makan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 02, 2023 at 02:53 PM
+-- Generation Time: Jul 19, 2023 at 09:53 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -40,15 +40,10 @@ CREATE TABLE `detail_pesanan` (
 --
 
 INSERT INTO `detail_pesanan` (`id_detail_pesanan`, `id_pesanan`, `id_menu`, `harga_tertentu`, `jumlah`) VALUES
-(174, 111, 10, 15000, 2),
-(175, 111, 12, 3000, 1),
-(176, 111, 13, 13000, 3),
-(195, 113, 10, 20000, 1),
-(196, 114, 12, 3000, 1),
-(197, 114, 10, 20000, 1),
-(198, 115, 12, 3000, 3),
-(199, 116, 10, 20000, 1),
-(200, 116, 12, 3000, 1);
+(217, 120, 12, 3000, 2),
+(219, 121, 10, 20000, 1),
+(226, 123, 11, 20000, 2),
+(227, 123, 12, 3000, 3);
 
 -- --------------------------------------------------------
 
@@ -78,6 +73,45 @@ INSERT INTO `karyawan` (`id_karyawan`, `username`, `password`, `email`, `nama_ka
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `label_pengeluaran`
+--
+
+CREATE TABLE `label_pengeluaran` (
+  `id_label` int(11) NOT NULL,
+  `label` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `label_pengeluaran`
+--
+
+INSERT INTO `label_pengeluaran` (`id_label`, `label`) VALUES
+(2, 'listrik');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `label_pesanan`
+--
+
+CREATE TABLE `label_pesanan` (
+  `id_label` int(11) NOT NULL,
+  `label` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `label_pesanan`
+--
+
+INSERT INTO `label_pesanan` (`id_label`, `label`) VALUES
+(6, 'a'),
+(4, 'listrik'),
+(2, 'offline'),
+(3, 'online');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `menu`
 --
 
@@ -101,6 +135,27 @@ INSERT INTO `menu` (`id_menu`, `kategori`, `nama_menu`, `harga`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `pengeluaran`
+--
+
+CREATE TABLE `pengeluaran` (
+  `id_pengeluaran` int(11) NOT NULL,
+  `keterangan` text DEFAULT NULL,
+  `nominal` int(11) NOT NULL,
+  `waktu` datetime NOT NULL,
+  `id_label` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pengeluaran`
+--
+
+INSERT INTO `pengeluaran` (`id_pengeluaran`, `keterangan`, `nominal`, `waktu`, `id_label`) VALUES
+(1, 'bayar listrik', 20000, '2023-07-03 10:54:41', 2);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `pesanan`
 --
 
@@ -110,19 +165,18 @@ CREATE TABLE `pesanan` (
   `uang_kembalian` int(11) NOT NULL,
   `total_bayar` int(11) NOT NULL,
   `waktu` datetime NOT NULL,
-  `waktu_update` datetime DEFAULT NULL
+  `waktu_update` datetime DEFAULT NULL,
+  `id_label` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `pesanan`
 --
 
-INSERT INTO `pesanan` (`id_pesanan`, `uang_pelanggan`, `uang_kembalian`, `total_bayar`, `waktu`, `waktu_update`) VALUES
-(111, 80000, 8000, 72000, '2023-06-30 08:24:06', '2023-07-02 08:52:44'),
-(113, 50000, 30000, 20000, '2023-07-02 14:28:28', NULL),
-(114, 25000, 2000, 23000, '2023-07-02 14:30:54', NULL),
-(115, 12000, 3000, 9000, '2023-07-02 15:04:48', NULL),
-(116, 50000, 27000, 23000, '2023-07-02 15:07:36', NULL);
+INSERT INTO `pesanan` (`id_pesanan`, `uang_pelanggan`, `uang_kembalian`, `total_bayar`, `waktu`, `waktu_update`, `id_label`) VALUES
+(120, 8000, 2000, 6000, '2023-07-05 07:48:35', NULL, 2),
+(121, 50000, 30000, 20000, '2023-07-01 07:49:19', '2023-07-03 07:49:47', 3),
+(123, 100000, 51000, 49000, '2023-07-19 14:45:52', '2023-07-19 14:53:26', 4);
 
 --
 -- Indexes for dumped tables
@@ -143,16 +197,38 @@ ALTER TABLE `karyawan`
   ADD PRIMARY KEY (`id_karyawan`);
 
 --
+-- Indexes for table `label_pengeluaran`
+--
+ALTER TABLE `label_pengeluaran`
+  ADD PRIMARY KEY (`id_label`),
+  ADD UNIQUE KEY `label` (`label`);
+
+--
+-- Indexes for table `label_pesanan`
+--
+ALTER TABLE `label_pesanan`
+  ADD PRIMARY KEY (`id_label`),
+  ADD UNIQUE KEY `label` (`label`);
+
+--
 -- Indexes for table `menu`
 --
 ALTER TABLE `menu`
   ADD PRIMARY KEY (`id_menu`);
 
 --
+-- Indexes for table `pengeluaran`
+--
+ALTER TABLE `pengeluaran`
+  ADD PRIMARY KEY (`id_pengeluaran`),
+  ADD KEY `FK__label_pengeluaran` (`id_label`);
+
+--
 -- Indexes for table `pesanan`
 --
 ALTER TABLE `pesanan`
-  ADD PRIMARY KEY (`id_pesanan`);
+  ADD PRIMARY KEY (`id_pesanan`),
+  ADD KEY `FK_pesanan_label_pesanan` (`id_label`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -162,13 +238,25 @@ ALTER TABLE `pesanan`
 -- AUTO_INCREMENT for table `detail_pesanan`
 --
 ALTER TABLE `detail_pesanan`
-  MODIFY `id_detail_pesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=201;
+  MODIFY `id_detail_pesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=228;
 
 --
 -- AUTO_INCREMENT for table `karyawan`
 --
 ALTER TABLE `karyawan`
-  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `label_pengeluaran`
+--
+ALTER TABLE `label_pengeluaran`
+  MODIFY `id_label` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `label_pesanan`
+--
+ALTER TABLE `label_pesanan`
+  MODIFY `id_label` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `menu`
@@ -177,10 +265,16 @@ ALTER TABLE `menu`
   MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
+-- AUTO_INCREMENT for table `pengeluaran`
+--
+ALTER TABLE `pengeluaran`
+  MODIFY `id_pengeluaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `pesanan`
 --
 ALTER TABLE `pesanan`
-  MODIFY `id_pesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=117;
+  MODIFY `id_pesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=124;
 
 --
 -- Constraints for dumped tables
@@ -192,6 +286,18 @@ ALTER TABLE `pesanan`
 ALTER TABLE `detail_pesanan`
   ADD CONSTRAINT `FK__menu` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id_menu`) ON UPDATE CASCADE,
   ADD CONSTRAINT `FK__pesanan` FOREIGN KEY (`id_pesanan`) REFERENCES `pesanan` (`id_pesanan`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pengeluaran`
+--
+ALTER TABLE `pengeluaran`
+  ADD CONSTRAINT `FK__label_pengeluaran` FOREIGN KEY (`id_label`) REFERENCES `label_pengeluaran` (`id_label`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD CONSTRAINT `FK_pesanan_label_pesanan` FOREIGN KEY (`id_label`) REFERENCES `label_pesanan` (`id_label`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
