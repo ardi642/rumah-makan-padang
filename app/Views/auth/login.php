9c26b39fba7edbc2c0dcaa6b4f884a499fb268d8
Ardiansyah Latif <ardiansyahlatif6420@gmail.com>
2023-08-05 10:37:26 +0700

    memperbarui fitur auth

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -1,9 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Rumah Makan Padang</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="" />
+    <meta
+      name="author"
+      content="Mark Otto, Jacob Thornton, and Bootstrap contributors"
+    />
+    <meta name="generator" content="Hugo 0.104.2" />
+    <title>Rumah Makan Nasi Padang</title>
+
+    <link
+      rel="canonical"
+      href="https://getbootstrap.com/docs/5.2/examples/sign-in/"
+    />
+
     <link href="../bootstrap/bootstrap.min.css" rel="stylesheet" />
 
     <script src="../jquery/jquery-3.6.0.min.js"></script>
@@ -17,74 +29,158 @@
     <script src="../select2/select2.min.js"></script>
     <script src="../axios/axios.min.js"></script>
     <script defer src="../alpinejs/alpinejs@3.x.x_dist_cdn.min.js"></script>
-    
-  </head>
 
-  <body>
-    <!-- Section: Design Block -->
-    <section class="text-center">
-      <!-- Background image -->
-      <div
-        class="p-5 bg-image"
-        style="
-          background-image: url('https://mdbootstrap.com/img/new/textures/full/171.jpg');
-          height: 300px;
-        "
-      ></div>
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
 
-      <div
-        class="card mx-4 mx-auto shadow-5-strong mb-5"
-        style="
-          margin-top: -100px;
-          background: hsla(0, 0%, 100%, 0.8);
-          backdrop-filter: blur(30px);
-          max-width: 600px;
-        "
-      >
-        <div class="card-body py-5" x-data>
-          <h2 class="fw-bold mb-5">Sign up now</h2>
-          <form id="form">
-            <div class="form-floating">
-              <input type="email" id="email" class="form-control" placeholder="name@example.com" name="password" x-model="$store.form.email">
-              <label for="email">Email</label>
-            </div>
-            <div class="form-floating mt-3">
-              <input type="password" id="password" class="form-control" placeholder="Password" name="password" x-model="$store.form.password">
-              <label for="password">Password</label>
-            </div>
-            <!-- Submit button -->
-            <button type="submit" class="btn btn-primary btn-block mt-4 py-3" style="width: 100%;" @click="handleSubmit">
-                  Sign up
-            </button>
-          </form>
-        </div>
-      </div>
-    </section>
-
-    <script>
-    document.addEventListener('alpine:init', () => {
-      Alpine.store('form', {
-        email: '',
-        password: ''
-      })
-      // Alpine.store('validasi', {})
-      // Alpine.store('state', {
-      //   loading: false,
-      //   status: null
-      // })
-    })
-      async function handleSubmit(e) {
-        e.preventDefault()
-        const form = document.getElementById('form')
-        const data = Alpine.store('form')
-
-        try {
-          const res = await axios.post('/api/login', data)
-          console.log(res)
-        } catch(error) {
-
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
         }
       }
-    </script>
+
+      .b-example-divider {
+        height: 3rem;
+        background-color: rgba(0, 0, 0, 0.1);
+        border: solid rgba(0, 0, 0, 0.15);
+        border-width: 1px 0;
+        box-shadow: inset 0 0.5em 1.5em rgba(0, 0, 0, 0.1),
+          inset 0 0.125em 0.5em rgba(0, 0, 0, 0.15);
+      }
+
+      .b-example-vr {
+        flex-shrink: 0;
+        width: 1.5rem;
+        height: 100vh;
+      }
+
+      .bi {
+        vertical-align: -0.125em;
+        fill: currentColor;
+      }
+
+      .nav-scroller {
+        position: relative;
+        z-index: 2;
+        height: 2.75rem;
+        overflow-y: hidden;
+      }
+
+      .nav-scroller .nav {
+        display: flex;
+        flex-wrap: nowrap;
+        padding-bottom: 1rem;
+        margin-top: -1px;
+        overflow-x: auto;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch;
+      }
+
+      html,
+      body {
+        height: 100%;
+      }
+
+      body {
+        display: flex;
+        align-items: center;
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #f5f5f5;
+      }
+
+      .form-signin {
+        max-width: 330px;
+        padding: 15px;
+      }
+
+      .form-signin .form-floating:focus-within {
+        z-index: 2;
+      }
+
+      .form-signin input[type="email"] {
+        margin-bottom: -1px;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+      }
+
+      .form-signin input[type="password"] {
+        margin-bottom: 10px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+      }
+    </style>
+
+    <!-- Custom styles for this template -->
+    <link href="signin.css" rel="stylesheet" />
+  </head>
+  <body class="text-center" x-data>
+    <main class="form-signin w-100 m-auto">
+      <div>
+        <img
+          class="mb-4" src="../logo/bootstrap-logo.svg" alt="" width="72" height="57"
+        />
+        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+        <div class="form-floating">
+          <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+          <label for="floatingInput">Email</label>
+        </div>
+        <div class="alert alert-danger mt-2 p-2" role="alert" x-show="$store.validasi.email != null">
+          Email Karyawan tidak terdaftar
+        </div>
+        <div class="form-floating">
+          <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+          <label for="floatingPassword">Password</label>
+        </div>
+        <div class="alert alert-danger mt-2 p-2" role="alert" x-show="$store.validasi.password != null">
+          Password tidak sesuai
+        </div>
+
+          <template x-if="!$store.state.loading">
+          <button class="w-100 btn btn-lg btn-primary" @click="">
+            <span>Masuk</span>
+          </button>
+          </template>
+          <template x-if="$store.state.loading">
+          <button class="w-100 btn btn-lg btn-primary">
+            <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+            proses...
+          </button>
+          </template>
+        <p class="mt-5 mb-3 text-muted">&copy; 2023</p>
+      </div>
+    </main>
   </body>
+  <script>
+    document.addEventListener("alpine:init", () => {
+      Alpine.store("validasi", {
+        email: null,
+        password: null,
+      });
+
+      Alpine.store("state", {
+        loading: false
+      })
+    });
+
+    try {
+      const storeState = Alpine.store('state')
+      storeState.loading = true
+    }
+    catch (error) {
+
+    }
+    finally {
+      storeState.loading = false
+    }
+
+  </script>
 </html>
